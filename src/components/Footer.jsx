3aacb9088615d46db9bd2833fa8caa9fc9a1ae62
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2024-10-17 22:54:16 +0200

    Base

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h3>John Doe</h3>
+      <ul>
+        <li>40 rue Laure Sieblod</li>
+        <li>69009 Lyon, France</li>
+        <li>Téléphone : 06 20 30 40 50</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
