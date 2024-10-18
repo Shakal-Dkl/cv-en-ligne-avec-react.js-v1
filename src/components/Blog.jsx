c8e603ad67d2843adfa4d6e3e740e45af0ef04be
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2024-10-18 13:17:39 +0200

    14

---
+++ src/components/Blog.jsx
@@ -0,0 +1,9 @@
+const Blog = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur ma page de Blog !</h1>
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Bienvenue sur ma page de Contact !</h1>
+    </div>
+  );
+};
+
+export default Contact;
