038bb63d764569bcd1d110e0ba0f34874a222166
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2024-11-01 21:44:42 +0100

    service

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
           </p>
         </div>
         <div className="row">
-          <article className="col-md-6 col-lg-6 mb-5">
+          <article className="fluid col-md-6 col-lg-6 mb-5 p-5">
             <h3>Mes coordonnées</h3>
             <address>
               <i className="fas fa-map-marker">
@@ -21,7 +21,7 @@ const Contact = () => {
             </address>
             <iframe
               title="map"
-              className="map-fluid"
+              className="map"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5565.313864755457!2d4.799070092087464!3d45.77806522783525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb65edac5b3f%3A0xe01c47049cb2e2b9!2s40%20Rue%20Laure%20Diebold%2C%2069009%20Lyon!5e0!3m2!1sfr!2sfr!4v1729716895766!5m2!1sfr!2sfr"
               width="600"
               height="450"
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,56 @@
+import Banner from "../assets/img/banner.jpg";
+
 const Services = () => {
   return (
     <div>
-      <div className="container" id="tittle">
+      <div className="banner" id="banniere">
+        <img className="img-fluid" src={Banner} alt="img_banner" />
+      </div>
+      <div
+        className="container justify-content-center text-center p-5"
+        id="tittle"
+      >
         <h1>MON OFFRE DE SERVICES</h1>
         <p>Voici les prestations sur lesquelles je peux intervenir.</p>
+      </div>
+      <div className="container text-center " id="main">
+        <div className="row justify-content-center  ">
+          <div className="shadow-sm rounded-4 bg-white col-md-6 col-lg-3 m-5 p-5">
+            <i
+              class="fa-solid fa-desktop fa-2xl"
+              style={{ color: "#0d6efd" }}
+            ></i>
+            <h2>UX DESIGN</h2>
+            <p>
+              L'<strong>UX Design</strong> est une methode de conception centrée
+              sur l'utilisateur. Son but est d'offrir une expérience de
+              navigation optimale à l'internaute.
+            </p>
+          </div>
+          <div className="shadow-sm rounded-4 bg-white col-md-6 col-lg-3 m-5 p-5">
+            <i
+              class="fa-solid fa-file-code fa-2xl"
+              style={{ color: "#0d6efd" }}
+            ></i>
+            <h2>DÉVELOPPEMENT WEB</h2>
+            <p>
+              Le<strong>développement de sites web</strong> repose sur
+              l'utilisation des langages HTML, CSS, JavaScript et PHP.
+            </p>
+          </div>
+          <div className="shadow-sm rounded-4 bg-white col-md-6 col-lg-3 m-5 p-5">
+            <i
+              class="fa-solid fa-magnifying-glass-dollar fa-2xl"
+              style={{ color: "#0d6efd" }}
+            ></i>
+            <h2>RÉFÉRENCEMENT</h2>
+            <p>
+              Le <strong>référencement naturel d'un site</strong>, aussi appelé
+              SEO, consiste à mettre des teccniques en oeuvre pour améliorer sa
+              position dans les résultats des moteurs de recherche.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
